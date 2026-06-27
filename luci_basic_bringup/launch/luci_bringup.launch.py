bfae0190ecf696479d13a49b5c5f2bd04cd47eaf
Kyshawn <kmsw8001@uw.edu>
2026-06-26 18:21:44 -0700

    Added luci_central_controller launch, removed twist_to_luci node

--- luci_basic_bringup/launch/luci_bringup.launch.py
+++ luci_basic_bringup/launch/luci_bringup.launch.py
@@ -22,6 +22,40 @@ def generate_launch_description():
         output='screen'
     )
 
+    ekf_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[{
+            'frequency': 50.0,
+            'sensor_timeout': 0.1,
+            'two_d_mode': True,
+            'odom0': '/odom',   # raw encoder odometry
+            'odom0_config': [True, True, False,
+                             False, False, True,
+                             True, False, False,
+                             False, False, False,
+                             False, False, False],
+            'imu0': '/luci/imu',
+            'imu0_config': [False, False, False,
+                            False, False, True,   # yaw orientation
+                            False, False, False,
+                            False, False, True,   # yaw velocity
+                            False, False, False],
+            'map_frame': 'map',           
+            'odom_frame': 'odom',         
+            'base_link_frame': 'base_link',
+            'world_frame': 'odom',         
+        }]
+    )
+
+    luci_control_node = Node(
+        package="luci_core_control",
+        executable="luci_state_manager_node",
+        name="luci_core_control_node",
+    )
+
     luci_odom_node = Node(
         package="luci_encoder_odometry",
         executable="encoder_to_odom_node",
@@ -33,8 +67,24 @@ def generate_launch_description():
         executable="quickie_500m_tf_node",
         name="quickie_500m_tf_node",
     )
+
+    # twist_to_luci_node = Node(
+    #     package='luci_basic_teleop',
+    #     executable='twist_to_luci',
+    #     name='twist_to_luci_node',
+    #     output='screen'
+    # )
+    central_controller_node = Node(
+        package='luci_core_control',
+        executable='luci_central_controller',
+        name='luci_central_controller_node',
+    )
+
     return LaunchDescription ([
         luci_grpc_node,
         luci_wheelchair_node,
-        luci_odom_node
+        luci_odom_node,
+        ekf_node,
+        luci_control_node,
+        central_controller_node,
     ])
